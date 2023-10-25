ca9a3fe8f3d0463c1486c55e3aaeae130b6ba0a9
ritikranjan12 <ritik123453@gmail.com>
2023-10-25 22:55:38 +0530

    created responsiveness navbar

--- ritikcommerce/src/components/Navbar.tsx
+++ ritikcommerce/src/components/Navbar.tsx
@@ -2,16 +2,16 @@
 import React from 'react'
 import Image from 'next/image';
 import { Button, Navbar } from 'flowbite-react';
- 
+
 const NavbarComponent = () => {
-    return (
-        <Navbar fluid rounded>
+  return (
+    <Navbar fluid rounded>
       <Navbar.Brand href="/">
         <Image width={36} height={18} src="/favicon.ico" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">NazCommerce</span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Button>Sign In / Sign Up</Button>
+        <Button className="hidden md:block">Sign In / Sign Up</Button>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
@@ -20,10 +20,10 @@ const NavbarComponent = () => {
         </Navbar.Link>
         <Navbar.Link href="#">About</Navbar.Link>
         <Navbar.Link href="#">Category</Navbar.Link>
-        
+        <Button className='block md:hidden'>Sign In / Sign Up</Button>
       </Navbar.Collapse>
     </Navbar>
-    )
+  )
 }
 
 export default NavbarComponent
